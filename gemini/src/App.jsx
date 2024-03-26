e46893cb0d5eb1d41c006bd4638e450165cc2aca
Pranjal Jaiswal <pranjaljaiswal35@gmail.com>
2024-03-26 12:17:17 +0530

    feat:add gemini and chatgpt file

--- gemini/src/App.jsx
+++ gemini/src/App.jsx
@@ -1,21 +1,34 @@
 import {GoogleGenerativeAI} from "@google/generative-ai"
 
 import './App.css'
+import { useState } from "react"
 
 function App() {
+  
+  const [result,setResult]=useState(null)
+  const [searchTxt,setSearchTxt]=useState("")
   const genAI=new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_PRO_API_KEY)
+  const handleClick=(e)=>{
+    e.preventDefault()
   async function getdata(){
     const model=genAI.getGenerativeModel({model:"gemini-pro"})
-    const prompt="Act as a Movie Recommendation system and suggest some movies for the query : indian horror. only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya"
+    const prompt=`Act as a Movie Recommendation system and suggest some movies for the query :${searchTxt} . only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya`
     const result=await model.generateContent(prompt);
     const response=await result.response;
-    const text=response.text
-    console.log(text)
+    const text=response.text()
+    const arr=text.split(", ")
+    setResult(arr)
   }
   getdata()
+  }
+  console.log(result)
   return (
     <>
-   
+    <input type="text" value={searchTxt} onChange={(e)=>setSearchTxt(e.target.value)}/>
+    <button onClick={handleClick}>click</button>
+     <ul>
+      {result && result.map((item,index)=>(<li key={index}>{item}</li>))}
+     </ul>
     </>
   )
 }
--- openai/src/App.jsx
+++ openai/src/App.jsx
@@ -4,14 +4,18 @@ import openai from './openai';
 import './App.css'
 
 function App() {
+  
+
   async function main() {
     const chatCompletion = await openai.chat.completions.create({
       messages: [{ role: 'user', content:  "Act as a Movie Recommendation system and suggest some movies for the query : indian horror. only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya" }],
       model: 'gpt-3.5-turbo',
     });
-    console.log(chatCompletion)
+    
+   const result= chatCompletion.choices[0].message.content
+  console.log(result)
   }
-main()
+  main()
 
 
   return (
